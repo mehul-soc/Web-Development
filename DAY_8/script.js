519a4933331910e5b186ec6dc4afb4913029d96f
mehul-soc <mehulj0620@gmail.com>
2026-06-15 22:14:35 +0530

    Day 8 started learning JS, Variables, Data Types, how to add JS

---
+++ DAY_8/script.js
@@ -0,0 +1,30 @@
+console.log("Padhlo ")
+
+let Mehul = "Yashwant";
+console.log(Mehul);
+
+Mehul = "Jain";
+console.log(Mehul);
+
+Mehul = 20
+// console.log(Mehul = "sep");
+console.log(`${Mehul} Sep`)  
+
+
+const Yashwant = "Mehul";
+console.log(Yashwant);
+
+// Yashwant = "Prasad";
+console.log(Yashwant)
+
+
+let a = 10
+{
+    a = 20
+    console.log(a);
+}
+console.log(a);
+
+var a = "Hello World"
+a = "Hello Buddy"
+console.log(a)
